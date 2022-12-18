8d4b9d6ff2c2166f00cae86e5a972cfa708b906a
yahoogm <yaho.garfield@gmail.com>
2022-12-18 18:14:40 +0700

    Add http request

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -1,1 +1,21 @@
-console.log("Halo, kita akan belajar membuat server");
+// * import core module http
+const http = require("http");
+
+// * function to listen the request and make response
+const requestListener = (request, response) => {
+  response.setHeader("Content-Type", "text/html");
+  response.statusCode = 200;
+  response.end("<h1>Hallo Http Server!</h1>");
+};
+
+// * create server
+const server = http.createServer(requestListener);
+
+// * create port and host
+const port = 5000;
+const host = "localhost";
+
+// * add instace to listen the server
+server.listen(port, host, () => {
+  console.log(`Server berjalan pada http://${host}:${port}`);
+});
