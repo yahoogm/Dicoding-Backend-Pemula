1e198d554c73f9ddfc03f47f2b4302366ad86278
yahoogm <yaho.garfield@gmail.com>
2022-12-31 20:02:58 +0700

    - Change response to json

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -13,15 +13,27 @@ const requestListener = (request, response) => {
   if (url === "/") {
     if (method === "GET") {
       response.statusCode = 200;
-      response.end("<h1>Ini adalah homepage</h1>");
+      response.end(
+        JSON.stringify({
+          message: "Ini adalah homepage",
+        })
+      );
     } else {
       response.statusCode = 400;
-      response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+      response.end(
+        JSON.stringify({
+          message: `Halaman tidak dapat diakses dengan ${method} request`,
+        })
+      );
     }
   } else if (url === "/about") {
     if (method === "GET") {
       response.statusCode = 200;
-      response.end("<h1>Hallo, ini adalah halaman about</h1>");
+      response.end(
+        JSON.stringify({
+          message: "Halo! Ini adalah halaman about",
+        })
+      );
     } else if (method === "POST") {
       let body = [];
 
@@ -41,7 +53,11 @@ const requestListener = (request, response) => {
     }
   } else {
     response.statusCode = 404;
-    response.end("<h1>Halaman tidak ditemukan</h1>");
+    response.end(
+      JSON.stringify({
+        message: "Halaman tidak ditemukan!",
+      })
+    );
   }
 };
 
