e3f13f9628be9b5edc7de5d91c2c0ebf134bf77f
yahoogm <yaho.garfield@gmail.com>
2023-01-01 10:37:33 +0700

    - Add path parameter

--- hapi-web-server/routes.js
+++ hapi-web-server/routes.js
@@ -32,6 +32,15 @@ const routes = [
   },
 
   {
+    method: "GET",
+    path: "/hello/{name?}",
+    handler: (request, h) => {
+      const { name = "stranger" } = request.params;
+      return `Hello, ${name}`;
+    },
+  },
+
+  {
     method: "*",
     path: "/{any*}",
     handler: (request, h) => {
