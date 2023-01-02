0836b543be3d0c391babda083a15e34f8f9911e5
yahoogm <yaho.garfield@gmail.com>
2023-01-02 21:58:36 +0700

    - Add method GET notes - Add method GET note/id

--- notes-app-back-end/src/routes.js
+++ notes-app-back-end/src/routes.js
@@ -1,10 +1,22 @@
-const { addNoteHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/notes',
     handler: addNoteHandler,
+  },
+
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+
+  {
+    method: 'GET',
+    path: '/notes{id}',
+    handler: getNoteByIdHandler,
   },
 ];
 
